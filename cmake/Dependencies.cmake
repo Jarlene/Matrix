180444208fcf980297c5dbb982b0333a6efd8b00
carresia <zhengshan0522@163.com>
2017-07-23 18:45:32 +0800

    修改openBlas的库引入方式

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,40 +1,5 @@
-# OpenBlas
-#if (USE_BLAS)
-#    SET(Open_BLAS_INCLUDE_SEARCH_PATHS
-#            /usr/include
-#            /usr/include/openblas
-#            /usr/include/openblas-base
-#            /usr/local/include
-#            /usr/local/include/openblas
-#            /usr/local/include/openblas-base
-#            /usr/local/OpenBLAS/include
-#            /usr/local/opt/openblas/include
-#            )
-#
-#    SET(Open_BLAS_LIB_SEARCH_PATHS
-#            /lib/
-#            /lib/openblas-base
-#            /lib64/
-#            /usr/lib
-#            /usr/lib/openblas-base
-#            /usr/lib64
-#            /usr/local/lib
-#            /usr/local/lib64
-#            /usr/local/OpenBLAS/lib
-#            /usr/local/opt/openblas/lib
-#            )
-#
-#    FIND_PATH(OpenBLAS_INCLUDE_DIR NAMES cblas.h PATHS ${Open_BLAS_INCLUDE_SEARCH_PATHS})
-#    FIND_LIBRARY(OpenBLAS_LIB NAMES openblas PATHS ${Open_BLAS_LIB_SEARCH_PATHS})
-#
-#    include_directories(${OpenBLAS_INCLUDE_DIR})
-#    link_libraries(${OpenBLAS_LIB})
-#    ADD_DEFINITIONS(-DUSE_BLAS)
-#endif ()
-
 #CUDA
 if (USE_CUDA)
-
     find_package(CUDA)
     if (CUDA_FOUND)
         #    message(STATUS "cude_include = ${CUDA_INCLUDE_DIRS}")
@@ -76,17 +41,7 @@ if (USE_MKL)
     ADD_DEFINITIONS(-DUSE_MKL)
 endif ()
 
-#OpenMp
-if (USE_MP)
-    FIND_PACKAGE(OpenMP REQUIRED)
-    if(OPENMP_FOUND)
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
-        set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
-        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
-        ADD_DEFINITIONS(-DUSE_MP)
-    endif()
-endif ()
+
 
 
 # JNI
@@ -95,18 +50,6 @@ endif ()
 #    include_directories(${JNI_INCLUDE_DIRS})
 #endif()
 
-#CUDA
-if (USE_CUDA)
-    find_package(CUDA)
-    if (CUDA_FOUND)
-        #    message(STATUS "cude_include = ${CUDA_INCLUDE_DIRS}")
-        #    message(STATUS "cude_lib = ${CUDA_LIBRARIES}")
-        include_directories(${CUDA_INCLUDE_DIRS})
-        link_libraries(${CUDA_LIBRARIES})
-        ADD_DEFINITIONS(-DUSE_CUDA)
-    endif()
-    add_subdirectory(${CMAKE_SOURCE_DIR}/cuda)
-endif ()
 
 #OpenCv
 if (USE_OPENCV)
--- cmake/external/blas.cmake
+++ cmake/external/blas.cmake
@@ -1,24 +1,41 @@
 if (USE_BLAS)
-    INCLUDE(ExternalProject)
     SET(BLAS_SOURCES_DIR ${THIRD_PARTY_PATH}/OpenBLAS)
     SET(BLAS_INSTALL_DIR ${THIRD_PARTY_PATH}/install/OpenBLAS)
     SET(BLAS_INCLUDE_DIR "${BLAS_INSTALL_DIR}/include" CACHE PATH "openblas include directory." FORCE)
+
+#    ExternalProject_Add(
+#            OpenBLAS
+#            ${EXTERNAL_PROJECT_LOG_ARGS}
+#            GIT_REPOSITORY "https://github.com/xianyi/OpenBLAS.git"
+#            GIT_TAG         "master"
+#            PREFIX          "${BLAS_SOURCES_DIR}"
+#            BUILD_COMMAND   "make"
+#            BUILD_IN_SOURCE 1
+#            INSTALL_DIR     "${BLAS_INSTALL_DIR}"
+#            INSTALL_COMMAND "make install"
+#    )
+
+    if (NOT EXISTS ${BLAS_SOURCES_DIR})
+        message(STATUS "git clone opeb blas library")
+        execute_process(
+                COMMAND  git clone https://github.com/xianyi/OpenBLAS.git ${BLAS_SOURCES_DIR}
+        )
+    endif ()
+
+    if (NOT EXISTS ${BLAS_INSTALL_DIR})
+        execute_process(
+                WORKING_DIRECTORY ${BLAS_SOURCES_DIR}
+                COMMAND make
+                COMMAND make install PREFIX=${BLAS_INSTALL_DIR}
+        )
+    endif ()
     IF(WIN32)
         SET(BLAS_LIBRARIES "${BLAS_INSTALL_DIR}/lib/libopenblas.lib")
     else(WIN32)
         SET(BLAS_LIBRARIES "${BLAS_INSTALL_DIR}/lib/libopenblas.a")
     endif(WIN32)
-    INCLUDE_DIRECTORIES(${BLAS_INSTALL_DIR})
-    ExternalProject_Add(
-            OpenBLAS
-            ${EXTERNAL_PROJECT_LOG_ARGS}
-            GIT_REPOSITORY "https://github.com/xianyi/OpenBLAS.git"
-            GIT_TAG  "master"
-            PREFIX          ${BLAS_SOURCES_DIR}
-            UPDATE_COMMAND  ""
-            CMAKE_ARGS      -DCMAKE_INSTALL_PREFIX:PATH=${BLAS_INSTALL_DIR}
-    )
-    LIST(APPEND external_project_dependencies OpenBLAS)
+    INCLUDE_DIRECTORIES(${BLAS_INCLUDE_DIR})
+#    LIST(APPEND external_project_dependencies OpenBLAS)
     LIST(APPEND external_libs ${BLAS_LIBRARIES})
     ADD_DEFINITIONS(-DUSE_BLAS)
 endif (USE_BLAS)
