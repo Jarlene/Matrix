4eed0c21904d92e511612a2b0164a95c61412808
魔才 <jarlene.xwl@alibaba-inc.com>
2017-12-15 17:50:22 +0800

    change update op decay

--- matrix/src/optimizer/MomentOptimizer.cpp
+++ matrix/src/optimizer/MomentOptimizer.cpp
@@ -18,6 +18,7 @@ namespace matrix {
             node->params["learning_rate"] = learning_rate;
             node->params["momentum"] = momentum;
             node->params["type"] = kMomentum;
+            node->params["decay"] = decay;
             node->inputs.push_back(it.first);
             node->inputs.push_back(it.second);
             it.first->outputs.push_back(std::weak_ptr<Node>(node));
