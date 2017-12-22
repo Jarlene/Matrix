57488562a040a8ab637e741e3ac7e635a95f239b
魔才 <jarlene.xwl@alibaba-inc.com>
2017-12-22 21:49:04 +0800

    remove mutex when countdown

--- matrix/src/base/Node.cpp
+++ matrix/src/base/Node.cpp
@@ -193,7 +193,6 @@ namespace matrix {
     }
 
     void Node::CountDown() {
-        std::lock_guard<std::mutex> lock(mutex);
         --depens;
         if (depens <= 0) {
             condvar.notify_all();
