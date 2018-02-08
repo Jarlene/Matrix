e4561dbd5f4f562e69ee9361420b68be498f5a73
魔才 <jarlene.xwl@alibaba-inc.com>
2018-02-08 18:10:37 +0800

    add mnist download with cmake

--- cmake/Download.cmake
+++ cmake/Download.cmake
@@ -12,5 +12,19 @@ function(matrix_download)
 
     if (NOT EXISTS ${DOWNLOAD_OUTPATH})
         FILE(DOWNLOAD ${DOWNLOAD_URL} ${DOWNLOAD_OUTPATH} SHOW_PROGRESS)
+        get_filename_component(FILE_EXT ${DOWNLOAD_OUTPATH} EXT)
+        if ("${FILE_EXT}" STREQUAL ".gz")
+            execute_process(
+                    COMMAND gunzip ${DOWNLOAD_OUTPATH}
+            )
+        elseif("${FILE_EXT}" STREQUAL ".zip")
+            execute_process(
+                    COMMAND unzip ${DOWNLOAD_OUTPATH}
+            )
+        elseif("${FILE_EXT}" STREQUAL ".tar")
+            execute_process(
+                    COMMAND tar -xvf ${DOWNLOAD_OUTPATH}
+            )
+        endif ()
     endif ()
 endfunction(matrix_download)
