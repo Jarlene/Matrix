dd6e503d7b1a8aaa7a37ab17aef82fcbf65c32a5
魔才 <jarlene.xwl@alibaba-inc.com>
2018-03-15 10:09:45 +0800

    add zeromq.cmake

---
+++ cmake/external/zeromq.cmake
@@ -0,0 +1,22 @@
+if (USE_ZMQ)
+    INCLUDE(ExternalProject)
+    SET(ZMQ_SOURCES_DIR ${THIRD_PARTY_PATH}/zmq)
+    SET(ZMQ_INSTALL_DIR ${THIRD_PARTY_PATH}/install/zmq)
+    SET(ZMQ_INCLUDE_DIR "${EIGEN_INSTALL_DIR}/include" CACHE PATH "zmp include directory." FORCE)
+    IF (WIN32)
+        SET(ZMQ_LIBRARIES "${ZMQ_INSTALL_DIR}/lib/libzmq.lib")
+    else (WIN32)
+        SET(ZMQ_LIBRARIES "${ZMQ_INSTALL_DIR}/lib/libzmq.a")
+    endif (WIN32)
+    INCLUDE_DIRECTORIES(${ZMQ_INCLUDE_DIR})
+    ExternalProject_Add(
+            zmq
+            ${EXTERNAL_PROJECT_LOG_ARGS}
+            GIT_REPOSITORY "https://github.com/zeromq/libzmq.git"
+            PREFIX ${ZMQ_SOURCES_DIR}
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${ZMQ_INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release
+    )
+    LIST(APPEND external_project_dependencies zmq)
+    LIST(APPEND external_libs ${ZMQ_LIBRARIES})
+    ADD_DEFINITIONS(-DUSE_ZMQ)
+endif (USE_ZMQ)
